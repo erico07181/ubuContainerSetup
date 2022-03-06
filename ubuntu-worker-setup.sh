697f8bca3224931cd641bad462588c535833ff56
NesquickRabbit1 <49778407+NesquickRabbit1@users.noreply.github.com>
2022-03-05 18:44:41 -0700

    Update ubuntu-worker-setup.sh

--- ubuntu-worker-setup.sh
+++ ubuntu-worker-setup.sh
@@ -11,6 +11,8 @@ apt-get install \
 	lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
+swapoff -a
+
 echo \
 	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -18,13 +20,6 @@ echo \
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 apt-get update && apt-get install -y apt-transport-https curl
-
-
-echo "{
-	\"exec-opts\":  [\"native.cgroupdriver=systemd\"]
-}" > /etc/docker/daemon.json
-systemctl daemon-reload
-systemctl restart docker
 
 apt install make
 snap install go --classic
